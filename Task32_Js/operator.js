cb7f816364208a8f4383155ed144dfb88cbf7fe6
AbhijeetKuhikar <abhijeetkuhikar015@gmail.com>
2025-08-07 23:49:53 +0530

    Javascript Begins with task32

---
+++ Task32_Js/operator.js
@@ -0,0 +1,16 @@
+let data = 100
+
+data += 10 
+data -= 5
+data *= 10 
+data /= 5 
+
+console.log(data)
+
+window.alert('hello')
+console.log('hello')
+
+let name = window.prompt("enter name")
+let age = 20
+
+console.log("hello ! " + name + " your age is " + age + " .")
