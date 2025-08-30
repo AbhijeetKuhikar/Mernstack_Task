c8a9ef177d10b8b9f55f8c70fa300ee4b674bc69
AbhijeetKuhikar <abhijeetkuhikar015@gmail.com>
2025-08-30 22:47:40 +0530

    Task 45

---
+++ Task45/script.js
@@ -0,0 +1,63 @@
+/* let tableData = [
+    {
+        name: "person 1",
+        phone: "123456"
+    },
+    {
+        name: "person 2",
+        phone: "123456"
+    },
+    {
+        name: "person 3",
+        phone: "123456"
+    },
+    {
+        name: "person 4",
+        phone: "123456"
+    }
+]
+
+
+let table = document.getElementById("table1")
+
+let tr = document.createElement("tr")
+let td1 = document.createElement("td")
+let td2 = document.createElement("td")
+
+td1.innerText = tableData[0].name
+td2.innerText = tableData[0].phone
+
+tr.appendChild(td1)
+tr.appendChild(td2)
+
+table.appendChild(tr)
+
+// table.append(tr) */
+let list = document.createElement("ul")
+
+let listItem1 = document.createElement("li")
+
+let listItem2 = document.createElement("li")
+
+let listItem3 = document.createElement("li")
+
+
+listItem1.innerHTML = "<b>this is some text</b>"
+listItem2.innerText = "item 2"
+listItem3.innerText = "item 3"
+
+listItem1.style.color = "red"
+
+list.appendChild(listItem1)
+list.appendChild(listItem2)
+list.appendChild(listItem3)
+
+document.getElementById("target").appendChild(list)
+
+document.getElementById("target").appendChild(list)
+
+document.getElementById("target").appendChild(list)
+
+let elementToRemove = document.getElementById("heading1")
+
+document.getElementById("target").removeChild(elementToRemove)
