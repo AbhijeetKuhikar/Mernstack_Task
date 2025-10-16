bf14625096df7f29c31f23aa2e3d4812528ad27f
AbhijeetKuhikar <abhijeetkuhikar015@gmail.com>
2025-10-16 22:49:41 +0530

    react-router-dom

---
+++ React/app6/app6/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import Service from "./components/pages/Service"
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/service/:servicename' element={<Service />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
+
